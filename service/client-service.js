c917dea495379c059dd0052dbe1ec115caa00ba2
Emmanuel Sanchez <fede@fede.website>
2023-04-01 22:20:17 -0600

    Creando templates

--- service/client-service.js
+++ service/client-service.js
@@ -1,3 +1,37 @@
+const crearNuevaLinea = (nombre, email) => {
+
+    const linea = document.createElement("tr");
+    const contenido = `<td class="td" data-td>
+    
+        ${nombre}
+            </td>
+            <td>${email}</td>
+            <td>
+              <ul class="table__button-control">
+                <li>
+                  <a
+                    href="../screens/editar_cliente.html"
+                    class="simple-button simple-button--edit"
+                    >Editar</a
+                  >
+                </li>
+                <li>
+                  <button
+                    class="simple-button simple-button--delete"
+                    type="button"
+                  >
+                    Eliminar
+                  </button>
+                </li>
+              </ul>
+            </td>`
+
+    linea.innerHTML = contenido;
+    return linea;
+};
+
+const table = document.querySelector("[data-table]")
+
 const http = new XMLHttpRequest();
 
 http.open("GET", "http://localhost:3000/perfil")
@@ -5,6 +39,16 @@ http.open("GET", "http://localhost:3000/perfil")
 http.send()
 
 http.onload = () => {
-    const data = http.response
-    console.log(data);
+    const data = JSON.parse(http.response)
+    data.forEach((perfil) => {
+        const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email)
+        table.appendChild(nuevaLinea);
+    });
+    const http2 = new XMLHttpRequest()
+    http2.open("GET", "http://localhost:3000/perfil/hoy")
+    http2.send()
+    http2.onload = () => {
+        const data2 = JSON.parse(http2.response)
+    }
+
 }
