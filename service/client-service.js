a45946312882399b5c5c3520eb06604ec68f420d
Emmanuel Sanchez <fede@fede.website>
2023-04-03 11:56:37 -0600

    Refaactorizando responsabilidades

--- service/client-service.js
+++ service/client-service.js
@@ -1,48 +1,10 @@
-const crearNuevaLinea = (nombre, email) => {
 
-    const linea = document.createElement("tr");
-    const contenido = `<td class="td" data-td>
-    
-        ${nombre}
-            </td>
-            <td>${email}</td>
-            <td>
-              <ul class="table__button-control">
-                <li>
-                  <a
-                    href="../screens/editar_cliente.html"
-                    class="simple-button simple-button--edit"
-                    >Editar</a
-                  >
-                </li>
-                <li>
-                  <button
-                    class="simple-button simple-button--delete"
-                    type="button"
-                  >
-                    Eliminar
-                  </button>
-                </li>
-              </ul>
-            </td>`
-
-    linea.innerHTML = contenido;
-    return linea;
-};
-
-const table = document.querySelector("[data-table]")
 
 const listaClientes = () => fetch("http://localhost:3000/perfil").then( (respuesta) => respuesta.json())
 
 
-listaClientes().then((data) => {
-    
-    data.forEach((perfil) => {
-        const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email)
-        table.appendChild(nuevaLinea);
-    });
-}).catch((error) => {
-    console.log(error);
-})
+export const clientServices = {
+  listaClientes,
+};
 
 
