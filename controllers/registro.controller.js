f6d8f27a9176479db088c5c6aba53fdd6c8b51dc
Emmanuel Sanchez <fede@fede.website>
2023-04-03 13:16:23 -0600

    Creando un cliente

---
+++ controllers/registro.controller.js
@@ -0,0 +1,13 @@
+import { clientServices } from "../service/client-service.js";
+const formulario = document.querySelector("[data-form]")
+
+formulario.addEventListener("submit", (evento) => {
+    evento.preventDefault();
+    const nombre =  document.querySelector("[data-nombre]").value
+    const email =  document.querySelector("[data-email]").value
+    console.log(nombre);
+    console.log(email);
+    clientServices.crearCliente(nombre, email).then(response => {
+        window.location.href = "/screens/registro_completado.html"
+    }).catch(err => console.log(err))
+})
