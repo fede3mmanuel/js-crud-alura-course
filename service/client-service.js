1cae546580d4fffaad0f59ea1d68e2cf38254f98
Emmanuel Sanchez <fede@fede.website>
2023-04-03 12:50:09 -0600

    update code

--- service/client-service.js
+++ service/client-service.js
@@ -1,10 +1,6 @@
-
-
-const listaClientes = () => fetch("http://localhost:3000/perfil").then( (respuesta) => respuesta.json())
-
+const listaClientes = () =>
+  fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
 
 export const clientServices = {
   listaClientes,
 };
-
-
