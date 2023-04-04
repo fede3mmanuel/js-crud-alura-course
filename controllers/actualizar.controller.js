6bd50fffca2bc2f45df6151614ebe026b519ccd5
Emmanuel Sanchez <fede@fede.website>
2023-04-03 18:56:01 -0600

    Async/Await

--- controllers/actualizar.controller.js
+++ controllers/actualizar.controller.js
@@ -2,7 +2,7 @@ import { clientServices } from "../service/client-service.js";
 
 const formulario = document.querySelector("[data-form]")
 
-const ObtenerInformacion = () => {
+const ObtenerInformacion = async () => {
     const url = new URL(window.location);
 
     const id = url.searchParams.get("id");
@@ -13,13 +13,9 @@ const ObtenerInformacion = () => {
 
     const nombre = document.querySelector("[data-nombre]")
     const email = document.querySelector("[data-email]")
-
-
-
-    clientServices.detalleCliente(id).then((perfil) => {
+    const perfil = await clientServices.detalleCliente(id)
         nombre.value = perfil.nombre
         email.value = perfil.email
-    })
 }
 
 ObtenerInformacion()
